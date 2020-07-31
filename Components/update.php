40c2781707ed36d8fcfdcc211d690a433a36c645
MEHMOOD <daniyalshafiq205@gmail.com>
2020-08-01 04:13:02 +0500

    FOntawesome issue solve commit

--- Components/update.php
+++ Components/update.php
@@ -61,7 +61,7 @@ if (isset($_REQUEST['update'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="screen" />
   <link rel="stylesheet" href="../fontawesome/css/all.css">
   <link rel="stylesheet" href="../css/bootstrap.min.css">
 
--- index.php
+++ index.php
@@ -61,10 +61,15 @@ if (isset($_REQUEST['submit'])) {
 
 <head>
     <meta charset="UTF-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="screen" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="icon" href="assets/icon/icon.ico" >
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+  
     <title>Form</title>
 </head>
 
@@ -267,13 +272,11 @@ background: linear-gradient(to left, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Fi
 
 
 
-    <?php
+    <?php $conn->close();?>
+
+ 
 
 
-    $conn->close();
-    ?>
-<script src="/js/bootstrap.js"></script>
-    <script src="/js/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
